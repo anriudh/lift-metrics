2e2338ca8ad02b6944f8c7e23496193d65cf9567
anriudh <anirudham.10b.svn@gmail.com>
2026-03-28 23:49:17 +0530

    add workout, exercise and profile models for pg

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=255)
+    muscle_group = models.CharField(max_length=100)
+    equipment = models.CharField(max_length=100)
+    image_url = models.URLField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workouts')
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, related_name='workouts')
+    sets = models.PositiveIntegerField()
+    reps = models.PositiveIntegerField()
+    weight = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField()
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.exercise.name} - {self.date}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
